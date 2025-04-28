444c5cfc6c8b3ac1b6d8b6f7ee7f025a931f03bb
petergyenge <peter.gyenge@gmail.com>
2025-04-28 16:53:55 +0200

    auth.tsx

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,27 +1,8 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import { db } from "../db.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { session } = await authenticate.admin(request);
-
-  console.log("🔥 afterAuth meghívva az auth.$.tsx loaderben!");
-  console.log("➡️ Shop:", session.shop);
-  console.log("🔐 Access Token:", session.accessToken);
-
-  try {
-    await db.shop.upsert({
-      where: { shopDomain: session.shop },
-      update: { accessToken: session.accessToken },
-      create: {
-        shopDomain: session.shop,
-        accessToken: session.accessToken ?? "default",
-      },
-    });
-    console.log("✅ Shop sikeresen mentve az adatbázisba!");
-  } catch (err) {
-    console.error("DB hiba:", err);
-  }
+  await authenticate.admin(request);
 
   return null;
 };
