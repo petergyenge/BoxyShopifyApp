3b5efb3e0d8be45a5d04e0f746adc49b3c046665
petergyenge <peter.gyenge@gmail.com>
2025-05-12 12:42:41 +0200

    add routes index

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,17 +1,19 @@
 import styles from "./styles.module.css";
 
 export default function App() {
-
   return (
     <div className={styles.index}>
       <div className={styles.content}>
-        <h1 className={styles.heading}>Lépj be a Boxy Portál felületére</h1>
+        <h1 className={styles.heading}>Boxy Portál</h1>
+        <p className={styles.subtext}>Kattints az alábbi hivatkozásra, ha meg szeretnéd nyitni a Boxy Portált.</p>
         <a
           href="https://b2bp-test.apps.boxygroup.hu/"
-          className={styles.button}
+          className={styles.link}
+          target="_blank"
         >
-          Belépés a Boxy Portálra
+          Ugrás a Boxy Portálra
         </a>
+
       </div>
     </div>
   );
